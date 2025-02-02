7adc3dfa03f53d9a0255b8f02b31e3d54189392a
Riva Jain <rivajain@Rivas-MacBook-Air.local>
2025-02-02 11:42:10 -0500

    finished utility page and added vishana's ai code

--- backend/ai_labeling.py
+++ backend/ai_labeling.py
@@ -1,13 +1,12 @@
 from openai import OpenAI
+import base64
 import os
 
 API_KEY = os.getenv("API_KEY")
 #def generate_label(image_path):
 def generate_label():
-    '''
     with open(image_file, "rb") as image_file:
         image = base64.b64encode(image_file.read()).decode("utf-8")
-    '''
     client = OpenAI(
         api_key=API_KEY
     )
@@ -20,11 +19,11 @@ def generate_label():
                 "content": [
                     {
                         "type": "text",
-                        "text": "Describe the object in this image in one word.",
+                        "text": "Describe the object in this image in one sentence.",
                     },
                     {
-                        "type": "image_url",
-                        "image_url": {"url": "https://i.redd.it/8bu29396j2a21.jpg"},  
+                        "type": "image_base64",
+                        "image_base64": image  # Send the base64-encoded image
                     },
                 ],
             }
@@ -37,3 +36,4 @@ def generate_label():
 
 
 print(generate_label())
+
